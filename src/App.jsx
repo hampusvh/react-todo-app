2c4719d89a6b6f73e00949b090a1f83aa28096fd
hampusvh <hampus.vh@gmail.com>
2025-02-11 21:45:49 +0100

    added state and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 
+import { useState } from "react";
+
 function App() {
-  return <h1>React Todo-App</h1>;     
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") return;
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <h1>React Todo-App</h1>
+      {/* Inputfält och knapp */}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Lägg till en uppgift..."
+        />
+        <button onClick={addTodo}>Lägg till
+        </button>
+
+        {/* Lista med uppgifter */}
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+  );     
 }
-export default App
+
+export default App;
